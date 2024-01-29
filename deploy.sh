627fbc3ba55e87445867b9c3d323de6dca5d4716
lukekoko <github@lukekoko.net>
2024-01-29 16:16:34 +1100

    chore: update deploy step

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # create .env file
+rm -f -- .env
 touch .env
 # add env variable to .env file
-echo $'REDDIT_CLIENTID=${REDDIT_CLIENTID}\n' >> .env
-echo $'REDDIT_CLIENTSECRET=${REDDIT_CLIENTSECRET}\n' >> .env
+echo "REDDIT_CLIENTID=$REDDIT_CLIENTID\n" >> .env
+echo "REDDIT_CLIENTSECRET=$REDDIT_CLIENTSECRET\n" >> .env
 
 # docker-compose up
 docker-compose pull
