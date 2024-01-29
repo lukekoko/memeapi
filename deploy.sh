55ee0ab692b49d6a76f38cfc4a29527124852514
lukekoko <github@lukekoko.net>
2024-01-29 16:08:32 +1100

    chore: update deploy step

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,6 @@ echo $'REDDIT_CLIENTID=${REDDIT_CLIENTID}\n' >> .env
 echo $'REDDIT_CLIENTSECRET=${REDDIT_CLIENTSECRET}\n' >> .env
 
 # docker-compose up
+docker-compose pull
+docker-compose up --force-recreate --build -d
+docker image prune -f
